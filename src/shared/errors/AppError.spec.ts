f2b16a9435d52ddb8afba19e76c1087936328dea
luizProgHenri <luiz.henrique@arenafantasy.com>
2024-08-08 11:01:51 -0300

    Ajustes nomenclatura AppErrors spec

--- src/shared/errors/AppError.spec.ts
+++ src/shared/errors/AppError.spec.ts
@@ -2,53 +2,53 @@ import { AppError, BadRequestError, NotFoundError, UnauthorizedError } from "./A
 
 describe("AppError", () => {
   test("should create an instance of AppError", () => {
-    const error = new AppError("Test");
-    expect(error).toBeInstanceOf(AppError);
+    const output = new AppError("Test");
+    expect(output).toBeInstanceOf(AppError);
   });
 
   test("should create an instance of AppError with default status code", () => {
-    const error = new AppError("Test");
-    expect(error.statusCode).toBe(500);
+    const output = new AppError("Test");
+    expect(output.statusCode).toBe(500);
   });
 
   test("should create an instance of AppError with custom status code", () => {
-    const error = new AppError("Test", 404);
-    expect(error.statusCode).toBe(404);
+    const output = new AppError("Test", 404);
+    expect(output.statusCode).toBe(404);
   });
 });
 
 describe("BadRequestError", () => {
   test("should create an instance of BadRequestError", () => {
-    const error = new BadRequestError("Test");
-    expect(error).toBeInstanceOf(BadRequestError);
+    const output = new BadRequestError("Test");
+    expect(output).toBeInstanceOf(BadRequestError);
   });
 
   test("should create an instance of BadRequestError with status code 400", () => {
-    const error = new BadRequestError("Test");
-    expect(error.statusCode).toBe(400);
+    const output = new BadRequestError("Test");
+    expect(output.statusCode).toBe(400);
   });
 });
 
 describe("NotFoundError", () => {
   test("should create an instance of NotFoundError", () => {
-    const error = new NotFoundError("Test");
-    expect(error).toBeInstanceOf(NotFoundError);
+    const output = new NotFoundError("Test");
+    expect(output).toBeInstanceOf(NotFoundError);
   });
 
   test("should create an instance of NotFoundError with status code 404", () => {
-    const error = new NotFoundError("Test");
-    expect(error.statusCode).toBe(404);
+    const output = new NotFoundError("Test");
+    expect(output.statusCode).toBe(404);
   });
 });
 
 describe("UnauthorizedError", () => {
   test("should create an instance of UnauthorizedError", () => {
-    const error = new UnauthorizedError("Test");
-    expect(error).toBeInstanceOf(UnauthorizedError);
+    const output = new UnauthorizedError("Test");
+    expect(output).toBeInstanceOf(UnauthorizedError);
   });
 
   test("should create an instance of UnauthorizedError with status code 401", () => {
-    const error = new UnauthorizedError("Test");
-    expect(error.statusCode).toBe(401);
+    const output = new UnauthorizedError("Test");
+    expect(output.statusCode).toBe(401);
   });
 });
