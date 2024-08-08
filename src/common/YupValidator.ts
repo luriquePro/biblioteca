d7d40c169269c94b7036c4c433daeb3ad378e7bd
luizProgHenri <luiz.henrique@arenafantasy.com>
2024-08-08 10:55:20 -0300

    Classe para erros da aplicação

--- src/common/YupValidator.ts
+++ src/common/YupValidator.ts
@@ -1,11 +1,12 @@
 import * as yup from "yup";
+import { AppError } from "../shared/errors/AppError";
 
 const YupValidator = async (dataShape: yup.AnyObject, dataValidation: object) => {
   try {
     const schema = yup.object().shape(dataShape);
     await schema.validate(dataValidation);
   } catch (error: any) {
-    throw new Error(error.message);
+    throw new AppError(error.message);
   }
 };
 
---
+++ src/shared/errors/AppError.ts
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 500) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
