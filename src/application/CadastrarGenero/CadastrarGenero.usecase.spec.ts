3a63bb96a001cb90c368732068fcb1465a9d0be9
luizProgHenri <luiz.henrique@arenafantasy.com>
2024-08-09 11:39:42 -0300

    Criando teste para usecase de genero

---
+++ src/application/CadastrarGenero/CadastrarGenero.usecase.spec.ts
@@ -0,0 +1,33 @@
+import { GerarId } from "../../common/GerarId";
+import { IGeneroRepositorio } from "./CadastrarGenero.interface";
+import { CadastrarGeneroUseCase } from "./CadastrarGenero.usecase";
+
+describe("CadastrarGeneroUseCase", () => {
+  const GeneroRepositorio: IGeneroRepositorio = {
+    cadastrar: jest.fn(),
+    buscarGenero: jest.fn().mockResolvedValue(null)
+  };
+
+  test("Deve-se cadastrar um novo gênero", async () => {
+    const sut = new CadastrarGeneroUseCase(GeneroRepositorio);
+    const output = await sut.handle({ genero: "gênero_valido" });
+
+    expect(output).toBeUndefined();
+
+    expect(GeneroRepositorio.cadastrar).toHaveBeenCalledTimes(1);
+    expect(GeneroRepositorio.cadastrar).toHaveReturnedWith(undefined);
+
+    expect(GeneroRepositorio.buscarGenero).toHaveBeenCalledTimes(1);
+  });
+
+  test("Deve-se retornar erro se o gênero já está cadastrado", async () => {
+    const cadastrarGeneroDTO = { genero: "genero_existente" };
+    const generoDTO = { ...cadastrarGeneroDTO, id: GerarId() };
+
+    (GeneroRepositorio.buscarGenero as jest.Mock).mockResolvedValue(generoDTO);
+
+    const sut = new CadastrarGeneroUseCase(GeneroRepositorio);
+    await expect(sut.handle(cadastrarGeneroDTO)).rejects.toThrow("Genero já cadastrado");
+    expect(GeneroRepositorio.cadastrar).toHaveBeenCalledTimes(0);
+  });
+});
