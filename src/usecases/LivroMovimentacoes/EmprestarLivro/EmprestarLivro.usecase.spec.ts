426589f6a1e3dacfb4cec9cbbdf01ca5cedad2df
luizProgHenri <luiz.henrique@arenafantasy.com>
2024-08-21 13:22:11 -0300

    Testes para emprestarlivros usecase

--- src/usecases/LivroMovimentacoes/EmprestarLivro/EmprestarLivro.usecase.spec.ts
+++ src/usecases/LivroMovimentacoes/EmprestarLivro/EmprestarLivro.usecase.spec.ts
@@ -14,6 +14,9 @@ describe("EmprestarLivroUseCase", () => {
   let mockUsuarioRepositorio: jest.Mocked<IUsuarioRepositorio>;
   let emprestarLivroUseCase: EmprestarLivroUseCase;
 
+  let livroDTO: ILivroDTO;
+  let usuarioDTO: IUsuarioDTO;
+
   beforeEach(() => {
     mockLivroRepositorio = {
       buscarLivro: jest.fn(),
@@ -31,60 +34,8 @@ describe("EmprestarLivroUseCase", () => {
       buscarUsuario: jest.fn()
     } as unknown as jest.Mocked<IUsuarioRepositorio>;
 
-    emprestarLivroUseCase = new EmprestarLivroUseCase(mockLivroMovimentacoesRepositorio, mockLivroRepositorio, mockUsuarioRepositorio);
-  });
-
-  // test("Deve-se emprestar um livro quando os dados forem válidos", async () => {
-  //   const id_livro = GerarId();
-  //   const id_usuario = GerarId();
-  //   const periodo_dias = 5;
-
-  //   const livroCadastrado = { id: id_livro, titulo: "Livro teste", valor_emprestimo_diario: 10 };
-  //   const usuarioCadastrado = { id: id_usuario, nome_completo: "Usuario teste" };
-
-  //   mockLivroRepositorio.buscarLivro.mockResolvedValue(livroCadastrado);
-  //   mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(usuarioCadastrado);
-  //   mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis.mockResolvedValue(1);
-
-  //   await emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias });
-
-  //   expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledWith({ id: id_livro });
-  //   expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledWith({ id: id_usuario });
-  //   expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledWith({ id_livro });
-  //   expect(mockLivroMovimentacoesRepositorio.emprestarLivro).toHaveBeenCalledWith({
-  //     id: expect.any(String),
-  //     usuario: { id: id_usuario, nome_completo: "Usuario teste" },
-  //     livro: {
-  //       id: id_livro,
-  //       titulo: "Livro teste",
-  //       valor_emprestimo: 50,
-  //       taxa_multa_diaria: 0
-  //     },
-  //     periodo_dias,
-  //     data_emprestimo: expect.any(Date),
-  //     data_previsao_devolucao: expect.any(Date)
-  //   });
-  // });
-
-  // test("Deve falhar se o livro não for encontrado", async () => {
-  //   const id_livro = GerarId();
-  //   const id_usuario = GerarId();
-  //   const periodo_dias = 5;
-
-  //   mockLivroRepositorio.buscarLivro.mockResolvedValue(null);
-
-  //   await expect(emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias })).rejects.toThrow(
-  //     new NotFoundError("Livro não encontrado")
-  //   );
-  // });
-
-  test("Deve falhar se o usuário não for encontrado", async () => {
-    const id_livro = GerarId();
-    const id_usuario = GerarId();
-    const periodo_dias = 5;
-
-    const livroDTO: ILivroDTO = {
-      id: id_livro,
+    livroDTO = {
+      id: GerarId(),
       titulo: "titulo_valido",
       autores: [{ autor: "autor_valido", id: "id_valido" }],
       generos: [{ genero: "genero_valido", id: "id_valido" }],
@@ -110,48 +61,8 @@ describe("EmprestarLivroUseCase", () => {
       taxa_multa_diaria: 0.01
     };
 
-    mockLivroRepositorio.buscarLivro.mockResolvedValue(livroDTO);
-    mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(null);
-
-    await expect(emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias })).rejects.toThrow(
-      new NotFoundError("Usuario não encontrado")
-    );
-  });
-
-  test("Deve falhar se não houver unidades disponíveis", async () => {
-    const id_livro = GerarId();
-    const id_usuario = GerarId();
-    const periodo_dias = 5;
-
-    const livroDTO: ILivroDTO = {
-      id: id_livro,
-      titulo: "titulo_valido",
-      autores: [{ autor: "autor_valido", id: "id_valido" }],
-      generos: [{ genero: "genero_valido", id: "id_valido" }],
-      autores_ids: ["id_valido"],
-      generos_ids: ["id_valido"],
-      editora: {
-        editora: "editora_valida",
-        id: "id_valida"
-      },
-      edicao: 1,
-      ISBN: "123",
-      quantidade: 1,
-      data_lancamento: moment().toDate(),
-      data_detalhes: {
-        dia: 20,
-        mes: 7,
-        ano: 24
-      },
-      descricao: "descricao-valida",
-      valor_de_compra: 1,
-      valor_de_venda: 1,
-      valor_emprestimo_diario: 1,
-      taxa_multa_diaria: 0.01
-    };
-
-    const UsuarioDTO: IUsuarioDTO = {
-      id: "id_valido",
+    usuarioDTO = {
+      id: GerarId(),
       nome_completo: "nome_validop",
       cpf: "cpf_valido",
       email: "email_valido",
@@ -161,12 +72,81 @@ describe("EmprestarLivroUseCase", () => {
       endereco: "endereco_valido"
     };
 
+    emprestarLivroUseCase = new EmprestarLivroUseCase(mockLivroMovimentacoesRepositorio, mockLivroRepositorio, mockUsuarioRepositorio);
+  });
+
+  test("Deve-se emprestar um livro quando os dados forem válidos", async () => {
+    const id_livro = GerarId();
+    const id_usuario = GerarId();
+    const periodo_dias = 5;
+
     mockLivroRepositorio.buscarLivro.mockResolvedValue(livroDTO);
-    mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(UsuarioDTO);
+    mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(usuarioDTO);
+    mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis.mockResolvedValue(5);
+
+    await emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias });
+
+    expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledWith({ id: id_livro });
+    expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledWith({ id: id_usuario });
+    expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledWith({ id_livro });
+
+    expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledTimes(1);
+    expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledTimes(1);
+    expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledTimes(1);
+    expect(mockLivroMovimentacoesRepositorio.emprestarLivro).toHaveBeenCalledTimes(1);
+  });
+
+  test("Deve falhar se o livro não for encontrado", async () => {
+    const id_livro = GerarId();
+    const id_usuario = GerarId();
+    const periodo_dias = 5;
+
+    mockLivroRepositorio.buscarLivro.mockResolvedValue(null);
+
+    await expect(emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias })).rejects.toThrow(
+      new NotFoundError("Livro não encontrado")
+    );
+
+    expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledTimes(1);
+    expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledTimes(0);
+    expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledTimes(0);
+    expect(mockLivroMovimentacoesRepositorio.emprestarLivro).toHaveBeenCalledTimes(0);
+  });
+
+  test("Deve falhar se o usuário não for encontrado", async () => {
+    const id_livro = GerarId();
+    const id_usuario = GerarId();
+    const periodo_dias = 5;
+
+    mockLivroRepositorio.buscarLivro.mockResolvedValue(livroDTO);
+    mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(null);
+
+    await expect(emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias })).rejects.toThrow(
+      new NotFoundError("Usuario não encontrado")
+    );
+
+    expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledTimes(1);
+    expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledTimes(1);
+    expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledTimes(0);
+    expect(mockLivroMovimentacoesRepositorio.emprestarLivro).toHaveBeenCalledTimes(0);
+  });
+
+  test("Deve falhar se não houver unidades disponíveis", async () => {
+    const id_livro = GerarId();
+    const id_usuario = GerarId();
+    const periodo_dias = 5;
+
+    mockLivroRepositorio.buscarLivro.mockResolvedValue(livroDTO);
+    mockUsuarioRepositorio.buscarUsuario.mockResolvedValue(usuarioDTO);
     mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis.mockResolvedValue(0);
 
     await expect(emprestarLivroUseCase.handle({ id_livro, id_usuario, periodo_dias })).rejects.toThrow(
       new BadRequestError("O Livro selecionado não possui unidades disponiveis.")
     );
+
+    expect(mockLivroRepositorio.buscarLivro).toHaveBeenCalledTimes(1);
+    expect(mockUsuarioRepositorio.buscarUsuario).toHaveBeenCalledTimes(1);
+    expect(mockLivroMovimentacoesRepositorio.quantidadeLivrosDisponiveis).toHaveBeenCalledTimes(1);
+    expect(mockLivroMovimentacoesRepositorio.emprestarLivro).toHaveBeenCalledTimes(0);
   });
 });
